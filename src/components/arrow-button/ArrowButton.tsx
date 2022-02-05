dab18f5fc26c3877d63f83c836ec05f4c90fa915
Kevin Bruton <kevin.jose.bs@gmail.com>
2022-02-05 16:53:57 +0100

    feat: add arrow button component

---
+++ src/components/arrow-button/ArrowButton.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './ArrowButton.scss';
+import arrowUpIcon from '../../assets/arrow-up-icon.svg';
+import arrowDownIcon from '../../assets/arrow-down-icon.svg';
+
+type Props = { text: string, onClick: Function };
+type State = { position: string };
+
+export class ArrowButton extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { position: 'closed' };
+  }
+
+  clicked() {
+    const newPosition = this.state.position === 'closed' ? 'open' : 'closed';
+    this.props.onClick({ position: newPosition });
+    this.setState({ position: newPosition });
+  }
+
+  render() {
+    return (
+      <div className="buttoncontainer">
+        <button type="button" className="buttoncontainer__button" onClick={this.clicked.bind(this)}>
+          <span className="button__text">{this.props.text}</span>
+          <img className="button__img" src={this.state.position === 'closed' ? arrowDownIcon : arrowUpIcon} alt="Button state"/>
+        </button>
+      </div>
+    );
+  }
+}
