d0184fe2ae7aa54307b033154f17b9fead21661d
Kevin Bruton <kevin.jose.bs@gmail.com>
2022-02-07 08:50:53 +0100

    feat: add Track Album and Artist interfaces

--- src/models/interfaces.ts
+++ src/models/interfaces.ts
@@ -12,3 +12,27 @@ export interface SearchQuery {
   advQuery: AdvQuery,
   mediaTypes: Array<string>
 }
+
+export interface Track {
+  id: string,
+  name: string,
+  previewUrl: string,
+  externalUrl: string,
+  imageUrl: string,
+  artist: string
+}
+
+export interface Album {
+  id: string,
+  name: string,
+  artist: string,
+  imageUrl: string,
+  externalUrl: string
+}
+
+export interface Artist {
+  id: string,
+  name: string,
+  imageUrl: string,
+  externalUrl: string
+}
